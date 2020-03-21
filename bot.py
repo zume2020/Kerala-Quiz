d1d99612dc6dc8f1587e28a466c5ea31ad4147bd
Sabu Siyad <ssiyad@tuta.io>
2020-03-21 23:53:05 +0530

    Reformat code

--- bot.py
+++ bot.py
@@ -11,10 +11,10 @@ import logging
 import json
 import requests
 import random
+
 from time import sleep
-from telegram import  ParseMode
-from telegram.ext import Updater, CommandHandler
-from telegram.ext import MessageHandler, Filters
+from telegram import ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, run_async
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -22,7 +22,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-print("starting engine.")
+logging.INFO("Starting Engine")
 
 score = {}
 
@@ -33,10 +33,14 @@ q_round = 0
 correct_answer = 0
 delay_flag = 0
 
+
+@run_async
 def start(update, context):
-    update.message.reply_text('Hi {}! Use /quiz to start quiz'.format(update.message.chat.first_name))
+    update.message.reply_text(
+        'Hi {}! Use /quiz to start quiz'.format(update.message.chat.first_name))
 
 
+@run_async
 def send_quiz(context):
     """Send the alarm message."""
     global answer
@@ -56,18 +60,15 @@ def send_quiz(context):
     try:
         x = pool.pop()
 
-
         question = x['question']
-        answer   = x['correct_answer'].lower()
+        answer = x['correct_answer'].lower()
         category = x['category']
 
         print(answer)
 
         job = context.job
 
-
         qLoop = 0
-        
 
         for x in range(4):
             if correct_answer == 0:
@@ -77,69 +78,71 @@ def send_quiz(context):
                 else:
                     hint = ""
 
-                q_message = "❓*QUESTION* _[{}]_ \n{} \n{}".format(category,question,hint)
+                q_message = "❓*QUESTION* _[{}]_ \n{} \n{}".format(
+                    category, question, hint)
                 context.bot.send_message(job.context,
-                    text=q_message,
-                    parse_mode=ParseMode.MARKDOWN)
+                                         text=q_message,
+                                         parse_mode=ParseMode.MARKDOWN)
                 if x == 3:
-                    noGuessMessage = "⛔️ Nobody guessed, Correct answer was *{}*".format(answer)
+                    noGuessMessage = "⛔️ Nobody guessed, Correct answer was *{}*".format(
+                        answer)
                     context.bot.send_message(job.context,
-                        text=noGuessMessage,
-                        parse_mode=ParseMode.MARKDOWN)
+                                             text=noGuessMessage,
+                                             parse_mode=ParseMode.MARKDOWN)
 
                 sleep(12)
             else:
                 print("Correct answer triggered")
         print(x)
-        correct_answer = 0 #check this
+        correct_answer = 0  # check this
         q_round += 1
-        print("q round ",q_round)
+        print("q round ", q_round)
         print(score)
 
         if q_round > 4:
             q_round = 0
-            #stoping the quiz
+            # stoping the quiz
 
             score_message = ""
-            sorted_score = sorted(score.items(), key=lambda x: x[1])    
+            sorted_score = sorted(score.items(), key=lambda x: x[1])
             print(sorted_score)
             for k, v in sorted_score:
                 print(k, v)
-                score_message += "{} {}\n".format(k,v)
+                score_message += "{} {}\n".format(k, v)
 
             context.bot.send_message(job.context, text=score_message)
             job.schedule_removal()
 
-
     except IndexError as e:
         job = context.job
-        print("q round while error is",q_round)
+        print("q round while error is", q_round)
         print(e)
         context.bot.send_message(job.context, text="stoped")
         job.schedule_removal()
 
 
-
-
+@run_async
 def set_quiz(update, context):
 
     chat_id = update.message.chat_id
     try:
         # Add job to queue and stop current one if there is a timer already
-        update.message.reply_text('🏁 *Round Starts*!',parse_mode=ParseMode.MARKDOWN)
+        update.message.reply_text(
+            '🏁 *Round Starts*!', parse_mode=ParseMode.MARKDOWN)
 
         if 'job' in context.chat_data:
             old_job = context.chat_data['job']
             old_job.schedule_removal()
-        new_job = context.job_queue.run_repeating(send_quiz, 2, context=chat_id)
+        new_job = context.job_queue.run_repeating(
+            send_quiz, 2, context=chat_id)
         context.chat_data['job'] = new_job
         # update.message.reply_text('question!')
-
 
     except (IndexError, ValueError):
         update.message.reply_text('error')
 
 
+@run_async
 def unset(update, context):
     """Remove the job if the user changed their mind."""
     if 'job' not in context.chat_data:
@@ -150,8 +153,7 @@ def unset(update, context):
     job.schedule_removal()
     # del context.chat_data['job']
 
-    update.message.reply_text('✋ *Stopped*!',parse_mode=ParseMode.MARKDOWN)
-
+    update.message.reply_text('✋ *Stopped*!', parse_mode=ParseMode.MARKDOWN)
 
 
 # def check(update, context):
@@ -162,6 +164,7 @@ def unset(update, context):
 #         context.bot.send_message(chat_id=update.effective_chat.id, text=answer_result)
 #         score.append([update.message.chat.first_name,0])
 
+@run_async
 def check(update, context):
     if update.message.text.lower() == answer:
         print("Correct answer")
@@ -173,26 +176,28 @@ def check(update, context):
 
         answer_result = "🍀 Yes, *{}*!\n\n🏆 {} +1".format(answer, f_name)
 
-        context.bot.send_message(chat_id=update.effective_chat.id, 
-            text=answer_result,
-            parse_mode=ParseMode.MARKDOWN)
+        context.bot.send_message(chat_id=update.effective_chat.id,
+                                 text=answer_result,
+                                 parse_mode=ParseMode.MARKDOWN)
 
         if f_name in score:
-            score[f_name] +=1
+            score[f_name] += 1
         else:
-            score[f_name] =1
+            score[f_name] = 1
 
 
-
+@run_async
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
+
 
 def getToken():
     with open('token.json') as f:
         data = json.load(f)
         token = data['token']
     return token
+
 
 def main():
     """Run bot."""
@@ -208,13 +213,10 @@ def main():
                                   pass_job_queue=True,
                                   pass_chat_data=True))
     dp.add_handler(CommandHandler("stop", unset, pass_chat_data=True))
-
     dp.add_handler(MessageHandler(Filters.text, check))
-
     dp.add_error_handler(error)
 
     updater.start_polling()
-
     updater.idle()
 
 
