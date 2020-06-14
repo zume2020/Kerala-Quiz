2b3777dd48de16ee1cf523cdd3d66b7d61295357
Sumithran <sumithran@outlook.in>
2020-06-14 17:32:56 +0530

    config vars

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-TOKEN = "TOKEN FROM BOTFATHER"
-DB_URI = "POSTGRES DATABASE URI"
+import os
+# For local env
+# TOKEN = "TOKEN FROM BOTFATHER"
+# DB_URI = "POSTGRES DATABASE URI"
+
+# For Production
+TOKEN = os.environ.get('TOKEN')
+DB_URI = os.environ.get('DATABASE_URL')
